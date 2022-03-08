322884a759794b5e5422a3fdfc28c4ebcf7c5de5
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-08 10:17:18 -0300

    a

--- first-lybrary-alura/index.js
+++ first-lybrary-alura/index.js
@@ -1,26 +1,28 @@
 const chalk = require("chalk");
 const fs = require("fs");
-const texto =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)";
 
-function extraiLinks() {
+function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const LinksExtraidos = regex.exec(texto);
-  console.log(LinksExtraidos);
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados.length === 0 ? "não há links" : arrayResultados;
 }
 
-function trataErro(err) {
-  throw new Error(chalk.redBright(err.code, "Algo de Errado não esta certo!"));
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
   try {
-    const text = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.greenBright(text));
-  } catch (err) {
-    trataErro(err);
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro);
   }
 }
-extraiLinks();
-//pegaArquivo("./arquivos/texto1.md");
+
+module.exports = pegaArquivo;
