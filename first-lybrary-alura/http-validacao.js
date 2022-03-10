c8bca35475bf5be2bb7d4408e025348eaf127d81
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-10 10:49:16 -0300

    a

--- first-lybrary-alura/http-validacao.js
+++ first-lybrary-alura/http-validacao.js
@@ -1,5 +1,13 @@
+//const fetch = require("node-fetch");
+
+function checaStatus(arrayURLs) {}
+function geraArrayDeURLS(arrayLinks) {
+  //object.values(objeto);
+  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+
 function validaURLs(arrayLinks) {
-  return arrayLinks;
+  return geraArrayDeURLS(arrayLinks);
 }
 
 module.exports = validaURLs;
