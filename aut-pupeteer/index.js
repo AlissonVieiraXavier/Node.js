c16456acfe466e777e4f9bc3bd9a759de938a66d
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-10 14:02:44 -0300

    a

--- aut-pupeteer/index.js
+++ aut-pupeteer/index.js
@@ -1,13 +1,22 @@
 const puppeteer = require("puppeteer");
 
+let urls = [
+  "https://www.npmjs.com/package/puppeteer",
+  "https://cursos.alura.com.br/formacao-front-end",
+  "https://github.com/puppeteer/puppeteer/blob/v13.5.1/docs/api.md#pagepdfoptions",
+  "https://github.com/AlissonVieiraXavier",
+];
+
 console.log("running");
 
-async function bot() {
+async function bot(url, posicao) {
+  //Mostrar procedimento?
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.npmjs.com/package/puppeteer");
-  await page.screenshot({ path: "img/example.png" });
+  //endereco https;
+  await page.goto(url);
+  await page.screenshot({ path: `img/example${posicao}.png` });
   await browser.close();
 }
 
-bot();
+bot(urls[0], "0");
