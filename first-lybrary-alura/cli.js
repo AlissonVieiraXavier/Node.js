ba353ad83fda71657ad74aba2f173e228bfe1138
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-10 12:04:42 -0300

    a

--- first-lybrary-alura/cli.js
+++ first-lybrary-alura/cli.js
@@ -1,15 +1,17 @@
-const chalk = require("chalk");
-const pegaArquivo = require("./index");
-const validaURLs = require("./http-validacao");
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const pegaArquivo = require('./index');
+const validaURLs = require('./http-validacao');
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-  if (caminho[3] === "validar") {
-    console.log(chalk.yellow("Links validados"), validaURLs(resultado));
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
   } else {
-    console.log(chalk.yellow("lista de links"), resultado);
+    console.log(chalk.yellow('lista de links'), resultado);
   }
 }
 
