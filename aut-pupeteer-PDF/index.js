2de91173446fec4acfe77030c11b812bc6d2a5db
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-10 15:25:17 -0300

    a

--- aut-pupeteer-PDF/index.js
+++ aut-pupeteer-PDF/index.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 let urls = [
   "https://www.npmjs.com/package/puppeteer",
   "https://cursos.alura.com.br/formacao-front-end",
-  "https://github.com/puppeteer/puppeteer/blob/v13.5.1/docs/api.md#pagepdfoptions",
+  "https://github.com/",
   "https://github.com/AlissonVieiraXavier",
   "https://www.youtube.com/",
   "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox",
@@ -26,7 +26,7 @@ async function realizaPrint() {
   for (let i = 0; i < urls.length; i++) {
     await bot(urls[i], i);
   }
-  console.log("Prints realizados. disponiveis na pasta 'img' ");
+  console.log("PDFs baixados. disponiveis na pasta 'pdfs' ");
 }
 
 realizaPrint();
