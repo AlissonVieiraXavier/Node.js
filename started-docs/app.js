ec35c69a8d48439a1e5bb05ca27c63feaf33c33c
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-04-19 15:58:23 -0300

    a

--- started-docs/app.js
+++ started-docs/app.js
@@ -1,14 +1,24 @@
-const http = require('http');
+/*const http = require('http')
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res)=>{
+    res.end("xuxu beleza");
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}: ${port}/`);
 })
+server.listen(port,hostname,()=>{
+  console.log("running")  
+})
+*/
+const express = require('express');
+const app = express();
+const path = require('path')
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+     res.sendFile(path.join(__dirname,"/index.html"))
+})
+
+app.use('/',router)
+app.listen(process.env.port || 3000);
