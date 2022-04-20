6e84c3738f5b25368c19734988343fb813286d36
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-04-20 12:10:17 -0300

    a

--- api-interface/index.js
+++ api-interface/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = 3000;
-
 const app = express();
 
-app.get('/all', (req,res)=>{
+let post = [{
+   id:"newPost",
+   title:"teste",
+   description:"..."
+}]
 
+
+app.get('/all', (req,res)=>{
+    res.json(JSON.stringify(post));
 })
+
+app.post('/new',(req,res)=>{} )
 
 app.listen(port, () =>{
     console.log(`Server running on ${port}`)
